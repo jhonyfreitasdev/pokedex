ab446154621f5282b51f3241c8e69ae5a6bf3506
Jhony Freitas <jhony00._@hotmail.com>
2023-04-13 01:01:48 -0300

    Colocando borda na div

--- src/features/home/pokemon-list/styles.js
+++ src/features/home/pokemon-list/styles.js
@@ -5,11 +5,12 @@ export const Main = styled.main`
     flex-direction: column;
     background-color: #ffffff75;
     align-items: center;
+    border: 15px solid #ce0628;
     overflow: auto;
     box-shadow: 0 5px 10px 2px #00000020;
     width: 60%;
     height: 88%;
-    margin: 0 auto;
+    margin: -14px auto 0 auto;
     padding: 30px 40px;
 `
 
