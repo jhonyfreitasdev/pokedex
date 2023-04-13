472d74cf9a2117482662d95c4610374482fad519
Jhony Freitas <jhony00._@hotmail.com>
2023-04-13 00:45:33 -0300

    Adicionando tema de typos dos pokemons

---
+++ src/constants/theme.js
@@ -0,0 +1,24 @@
+export const theme = {
+    pokemonTypesBackground: {
+        normal: "#FFE68Baa",
+        grass: "#2b8b2daa",
+        fire: "#eb682fde",
+        water: "#5A8BCDaa",
+        electric: "#F6D43Cdd",
+        ground: "#885231ab",
+        bug: "#69d63aaa",
+        flying: "#B5B3DCaa",
+        fighting: "#CD8B5Ade",
+        poison: "#624A7Bcc",
+        ice: " #2C88B0ba",
+        dark: "#343E3Aaa",
+        steel: "#7AA38Eaa",
+        rock: "#A5A58Daa",
+        psychic: "#EE9CACee",
+        ghost: "#392939aa",
+        shadow: "#131313aa",
+        fairy: "#AC52ACae",
+        dragon: "#6F8DC7aa",
+        unknown: ""
+    }
+}
