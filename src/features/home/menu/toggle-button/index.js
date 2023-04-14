8598999e2eafc19b0652b77b6b66c5d9496e5781
Jhony Freitas <jhony00._@hotmail.com>
2023-04-13 23:32:45 -0300

    Removendo menu do features

--- src/features/home/menu/toggle-button/index.js
+++
@@ -1,7 +0,0 @@
-import { Button } from "./styles"
-
-export const ToggleButton = () => {    
-    return(            
-        <Button type="button">  </Button>
-    )
-}
--- src/features/home/menu/toggle-button/styles.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const Button = styled.button`
-    width: 40px;
-    height: 20px;
-`
