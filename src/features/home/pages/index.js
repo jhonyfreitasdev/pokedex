9aeb7eb17340f8254a6520b7e5bf5dd004d8dc63
Jhony Freitas <jhony00._@hotmail.com>
2023-04-13 23:33:19 -0300

    Atualizando componente home com novo caminho

--- src/features/home/pages/index.js
+++ src/features/home/pages/index.js
@@ -1,12 +1,16 @@
-import { Menu } from "../menu/pages";
+import { Menu } from "../../../components/menu/pages/index";
+import { Content } from "../../../components/content";
 import { PokemonList } from "../pokemon-list";
+
 import { Container } from "./styles";
 
 export const Home = () => {
     return(
         <Container>
             <Menu />
-            <PokemonList />
+            <Content> 
+                <PokemonList />
+            </Content>
         </Container>
     )
 }
