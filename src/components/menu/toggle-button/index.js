e843a184fdb752dff00147320d0afe9b72e49c4f
Jhony Freitas <jhony00._@hotmail.com>
2023-04-19 23:36:42 -0300

    Criando o React Switch

--- src/components/menu/toggle-button/index.js
+++ src/components/menu/toggle-button/index.js
@@ -1,7 +1,22 @@
-import { Button } from "./styles"
+import { useContext } from "react"
+import { themes, ThemeContext } from "../../../context/theme-context"
+import Switch from "react-switch"
+
+import { Dark, Light } from "./styles"
+import "./index.css"
 
 export const ToggleButton = () => {    
+
+    const { theme, toggleTheme } = useContext(ThemeContext)
+
     return(            
-        <Button type="button">  </Button>
+        <Switch 
+            onChange={toggleTheme} 
+            checked={theme === themes.light}
+            uncheckedIcon={<Dark> <i className="fa-solid fa-moon"></i> </Dark>}
+            checkedIcon={<Light> <i className="fa-solid fa-sun"></i> </Light>}
+            onColor="#ccc"
+            className="switch"
+        />
     )
 }
