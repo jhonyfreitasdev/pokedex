c2a7c80c34ca16c03cff4dfdddcd5800b2da8f53
Jhony Freitas <jhony00._@hotmail.com>
2023-04-19 23:31:54 -0300

    Criando o contexto do toggle theme

---
+++ src/context/theme-context.js
@@ -0,0 +1,32 @@
+import { createContext, useState } from "react";
+
+import dayBackground from "../_assets/images/day-background.jpg"
+import nightBackground from "../_assets/images/night-background.jpg"
+
+export const themes = {
+    light: {
+        backgroundImage: dayBackground,
+        backgroundColor: "#ffffff75"
+    },
+    dark: {
+        backgroundImage: nightBackground,
+        backgroundColor: "#00000075"
+    }
+}
+
+export const ThemeContext = createContext()
+
+export const BackgroundProvider = (props) => {
+
+    const [theme, setTheme] =useState(themes.light)
+
+    const toggleTheme = () => {
+        setTheme((curr) => curr === themes.light? themes.dark : themes.light)
+    }
+
+    return(
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+            {props.children}
+        </ThemeContext.Provider>
+    )
+}
