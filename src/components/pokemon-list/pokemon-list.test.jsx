3fc926de64b5b24c9f95f13dd9e83b1d8a99284c
Jhony Freitas <jhony00._@hotmail.com>
2023-04-29 01:20:36 -0300

    Teste sugerido pelo guga

--- src/components/pokemon-list/pokemon-list.test.jsx
+++ src/components/pokemon-list/pokemon-list.test.jsx
@@ -1,32 +1,20 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom"
-import Enzyme, { shallow } from "enzyme"
-import Adapter from '@wojtekmaj/enzyme-adapter-react-15';
+
 import { PokemonList } from "."
-
-Enzyme.configure({ adapter: new Adapter() });
-
-const mockNavigate = jest.fn()
-jest.mock('react-router', () => ({
-    ...jest.requireActual('react-router'),
-    useNavigate: () => mockNavigate
-}))
 
 describe("PokemonList", () => {
     it("should render correctly", () => {
-        render(<PokemonList />)
+        const { getByText } = render(<PokemonList />)
 
-        expect(screen.getByText("Carregar mais Pokemon...")).toBeInTheDocument()
+        expect(getByText("Carregar mais Pokemon...")).toBeInTheDocument()
     })
 
-    it("should call pokemon page when click the button", () => {
-        //usar o find pra pegar os botões
-    })
-
-    it("should increment state when click the button", () => {
+    it('It should return the correct status value', () => {
+        render(<PokemonList />);
+        const countLimit = screen.getByTestId('countLimit')
         
-        const wrapper = shallow(<PokemonList />)
-        expect(wrapper.state(countLimit)).toEqual(10)
+        expect(countLimit.textContent).toBe(10);
     })
 })
