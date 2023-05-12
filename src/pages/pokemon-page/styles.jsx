cbd595f35a358742a1a8f110eb9d0aaffe15ed34
Jhony Freitas <jhony00._@hotmail.com>
2023-05-11 23:39:17 -0300

    Atualizando padding da página

--- src/pages/pokemon-page/styles.jsx
+++ src/pages/pokemon-page/styles.jsx
@@ -3,9 +3,10 @@ import styled from "styled-components";
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: flex-start;
     background: url(${props => props.theme.backgroundImage}) center bottom no-repeat ;
     transition: .6s ease-in-out;
     background-size: cover;
     height: 100vh;
+    padding-top: 3vh;
 `
