52ec7237fd6d3c8315ca10962a68f8f8367fc91f
Jhony Freitas <jhony00._@hotmail.com>
2023-05-12 22:12:37 -0300

    Feito filtro das descrições dos movimentos

--- src/components/pokemon-details/index.jsx
+++ src/components/pokemon-details/index.jsx
@@ -12,7 +12,7 @@ export const PokemonDetails = () => {
     const [list, setList] = useState()
     const [buttonActive, setButtonActive] = useState()
 
-    const {name } = useParams()
+    const { name } = useParams()
     const pokeDetailsUrl = `https://pokeapi.co/api/v2/pokemon/${name}`
 
     useEffect(() => {
@@ -35,15 +35,19 @@ export const PokemonDetails = () => {
         const pokemonData = await getPokemons(pokeDetailsUrl)
         const movesNameList = pokemonData.moves.map(item => item.move.name)
 
-        const movesList = await Promise.all (movesNameList.map(move => {
+        const movesList = await Promise.all(movesNameList.map(move => {
             const getMoveDetails = async () => {
                 const moveData = await getPokemons(`https://pokeapi.co/api/v2/move/${move}`)
-
-                return {
-                    name: moveData.name,
-                    description: moveData.flavor_text_entries[2].flavor_text,
-                    type: moveData.type.name
+                const filteredDescription = moveData.flavor_text_entries.find(obj => obj.language.name === 'en')
+                
+                let object
+                if (filteredDescription === undefined) {
+                    object = { name: moveData.name, description: 'No description'}
+                }else{
+                    object = { name: moveData.name, description: filteredDescription.flavor_text }
                 }
+
+                return object
             }
 
             return getMoveDetails()
@@ -57,7 +61,7 @@ export const PokemonDetails = () => {
         const pokemonData = await getPokemons(pokeDetailsUrl)
         const abilitiesNameList = pokemonData.abilities.map(item => item.ability.name)
 
-        const abilityList = await Promise.all (abilitiesNameList.map(ability => {
+        const abilityList = await Promise.all(abilitiesNameList.map(ability => {
 
             const getAbilityDetails = async () => {
                 const abilityData = await getPokemons(`https://pokeapi.co/api/v2/ability/${ability}`)
@@ -70,7 +74,7 @@ export const PokemonDetails = () => {
 
             return getAbilityDetails()
         }))
-        
+
         setList(abilityList)
         setButtonActive("abilities")
     }
@@ -79,60 +83,64 @@ export const PokemonDetails = () => {
         <Container pokemon={pokemon}>
             {
                 pokemon !== undefined ?
-                <>
-                    <Link to="/"> X </Link>
+                    <>
+                        <Link to="/"> X </Link>
 
-                    <Card pokemon={pokemon}>
-                        <ContainerImage>
-                            <img src={pokemon.image} alt={`Imagem do ${pokemon.name}`} />
-                        </ContainerImage>
+                        <Card pokemon={pokemon}>
+                            <ContainerImage>
+                                <img src={pokemon.image} alt={`Imagem do ${pokemon.name}`} />
+                            </ContainerImage>
 
-                        <Description>
-                            <Title pokemon={pokemon}>{pokemon.name}</Title>
+                            <Description>
+                                <Title pokemon={pokemon}>{pokemon.name}</Title>
 
-                            <Type pokemon={pokemon}>
-                                {
-                                    pokemon.types.length === 2 ? 
+                                <Type pokemon={pokemon}>
+                                    {
+                                        pokemon.types.length === 2 ?
+                                            <>
+                                                <img src={types.pokemonImageBackground[pokemon.types[0]]} alt={pokemon.types[0]} />
+                                                <img src={types.pokemonImageBackground[pokemon.types[1]]} alt={pokemon.types[1]} />
+                                            </>
+                                            : <img src={types.pokemonImageBackground[pokemon.types[0]]} alt={pokemon.types[0]} />
+                                    }
+                                </Type>
+                            </Description>
+                        </Card>
+
+                        <ContainerButtons>
+                            <Button pokemon={pokemon} type="button" status={buttonActive === "moves" ? true : false} onClick={showMoves}>
+                                Moves
+                            </Button>
+
+                            <Button pokemon={pokemon} type="button" status={buttonActive === "abilities" ? true : false} onClick={showAbilities}>
+                                Abilities
+                            </Button>
+                        </ContainerButtons>
+
+                        <List pokemon={pokemon}>
+                            {
+                                list !== undefined ?
                                     <>
-                                        <img src={types.pokemonImageBackground[pokemon.types[0]]} alt={pokemon.types[0]}/>
-                                        <img src={types.pokemonImageBackground[pokemon.types[1]]} alt={pokemon.types[1]}/>
+                                        {
+                                            list.map((item, index) => {
+                                                return (
+                                                    <Item pokemon={pokemon} key={index}>
+                                                        <h3>{item.name}</h3>
+                                                        {
+                                                            item.description !== undefined ?
+                                                            <p>{item.description}</p>
+                                                            : ""
+                                                        }
+                                                    </Item>
+                                                )
+                                            })
+                                        }
                                     </>
-                                    : <img src={types.pokemonImageBackground[pokemon.types[0]]} alt={pokemon.types[0]}/>
-                                }
-                            </Type>
-                        </Description>
-                    </Card>
-
-                    <ContainerButtons>
-                        <Button pokemon={pokemon} type="button" status={buttonActive === "moves" ? true : false} onClick={showMoves}>
-                            Moves 
-                        </Button>
-
-                        <Button pokemon={pokemon} type="button" status={buttonActive === "abilities" ? true : false} onClick={showAbilities}> 
-                            Abilities 
-                        </Button>
-                    </ContainerButtons>
-
-                    <List pokemon={pokemon}>
-                        {
-                            list !== undefined ?
-                            <>
-                                {
-                                    list.map((item, index) => {
-                                        return (
-                                            <Item pokemon={pokemon} key={index}>
-                                                <h3>{item.name}</h3>
-                                                <p>{item.description}</p>
-                                            </Item>
-                                        )
-                                    })
-                                }
-                            </>
-                            : <Message> Selecione no botão a lista que deseja ver </Message>
-                        }
-                    </List>
-                </>
-                : ""
+                                    : <Message> Selecione no botão a lista que deseja ver </Message>
+                            }
+                        </List>
+                    </>
+                    : ""
             }
         </Container>
     )
