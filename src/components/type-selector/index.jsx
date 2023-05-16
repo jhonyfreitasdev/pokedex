0deb6858b2a44e8c124760916ae3d0761e77d7bb
Jhony Freitas <jhony00._@hotmail.com>
2023-05-15 22:38:56 -0300

    Criando o select de de types

--- src/components/type-selector/index.jsx
+++ src/components/type-selector/index.jsx
@@ -1,29 +1,36 @@
 import { useContext } from "react";
 import { FilterContext } from "../../context/filter-context";
 
+import { Select, Label } from "./styles"
+
 export const TypeSelector = () => {    
     const {type, setType} = useContext(FilterContext)
 
     return(
-        <select value={type} onChange={e => setType(e.target.value)}>
-            <option value="all">Todos</option>
-            <option value="bug">Bug</option>
-            <option value="dark">Dark</option>
-            <option value="dragon">Dragon</option>
-            <option value="electric">Electric</option>
-            <option value="fairy">Fairy</option>
-            <option value="fighting">Fighting</option>
-            <option value="fire">Fire</option>
-            <option value="flying">Flying</option>
-            <option value="ghost">Ghost</option>
-            <option value="grass">Grass</option>
-            <option value="ice">Ice</option>
-            <option value="normal">Normal</option>
-            <option value="poison">Poison</option>
-            <option value="psychic">Psychic</option>
-            <option value="rock">Rock</option>
-            <option value="steel">Steel</option>
-            <option value="water">Water</option>
-        </select>
+        <>
+            <Label htmlFor="types" > 
+            </Label>
+
+            <Select id="types" value={type} onChange={e => setType(e.target.value)}>
+                <option value="all">Todos</option>
+                <option value="steel">Aço</option>
+                <option value="water">Água</option>
+                <option value="dragon">Dragão</option>
+                <option value="electric">Elétrico</option>
+                <option value="fairy">Fada</option>
+                <option value="ghost">Fantasma</option>
+                <option value="fire">Fogo</option>
+                <option value="ice">Gelo</option>
+                <option value="grass">Grama</option>
+                <option value="bug">Inseto</option>
+                <option value="fighting">Lutador</option>
+                <option value="normal">Normal</option>
+                <option value="rock">Pedra</option>
+                <option value="psychic">Psíquico</option>
+                <option value="dark">Sombrio</option>
+                <option value="poison">Venenoso</option>
+                <option value="flying">Voador</option>
+            </Select>
+        </>
     )
 }
